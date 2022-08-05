b23fb636df85112133b0c78bc0b06de752595e9d
thompson <so.wo@foxmail.com>
2022-08-05 15:10:13 +0800

    fix：更新模板消息状态更新接口

--- src/Constants/TemplateMsgStatus/Approve.php
+++ src/Constants/TemplateMsgStatus/Approve.php
@@ -3,10 +3,11 @@
 namespace KyyIM\Constants\TemplateMsgStatus;
 
 class Approve {
-    const APPROVE_AGREE = 1;
-    const APPROVE_TRANSFER = 2;
-    const APPROVE_CHANGE = 3;
-    const APPROVE_REFUSE = 4; //
+    const APPROVE_AGREE = 1; //已同意
+    const APPROVE_TRANSFER_SIGN = 2; //转签
+    const APPROVE_CHANGE = 3; //转交
+    const APPROVE_REFUSE = 4; //拒绝
     const APPROVE_ADD_SIGN = 5; //加签
-    const APPROVE_CANCEL = 6; //撤销
+    const APPROVE_BACK = 6; //回退
+    const APPROVE_REVOKE = 7; //撤销
 }
--- src/Template/MessageType.php
+++ src/Template/MessageType.php
@@ -29,14 +29,13 @@ abstract class MessageType {
 
     /**
      * 更新消息状态
-     * @param int|string|array $scenes
      * @param int $status
      * @param int $operator
      * @param int $relation_id
      * @param string $relation_field
      * @return void
      */
-    public static function updateStatusByScene($scenes, int $status, int $operator, int $relation_id, string $relation_field = "data.id"){
+    public static function updateStatusByScene(int $status, int $operator, int $relation_id, string $relation_field = "id"){
         (new TemplateMessageService())->updateStatus(...func_get_args());
     }
 
